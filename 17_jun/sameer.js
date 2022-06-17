4f6ece98ac2c67d989c9cdff1d68d542fb1c7d95
Sameer <sameer.writes.70@gmail.com>
2022-06-17 14:48:02 +0530

    new file added

---
+++ 17_jun/sameer.js
@@ -0,0 +1,13 @@
+// console.log("working");
+// var a = 20;
+// var b = 20;
+// console.log(a);
+// console.log(b);
+// console.log(a + b);
+// let c;
+// c = process.argv[6];
+// console.log(c);
+
+let a = parseInt(Process.argv[2]);
+let b = parseInt(Process.argv[3]);
+console.log(a + b);
