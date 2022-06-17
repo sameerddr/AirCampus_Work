ed612ca8a4efc467f799c9279697a5c0d061b0af
Sameer <sameer.writes.70@gmail.com>
2022-06-17 19:37:24 +0530

    done

--- 17_jun/sameer.js
+++ 17_jun/sameer.js
@@ -15,6 +15,37 @@
 // console.log(a * b);
 // console.log(a / b);
 
-for (let i = 0; i <= 10; i++) {
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let a = 1;
+// while (a <= 10) {
+//     console.log(a);
+//     a++;
+// }
+
+let a = parseInt(process.argv[2]);
+for (i = 1; i <= a; i++) {
     console.log(i);
 }
+
+for (i = 1; i <= a; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+
+}
+
+
+let rev = 0;
+let num = 123456;
+let lastDigit;
+
+while (num != 0) {
+    lastDigit = num % 10;
+    rev = rev * 10 + lastDigit;
+    num = Math.floor(num / 10);
+}
+
+console.log("Reverse number : " + rev);
