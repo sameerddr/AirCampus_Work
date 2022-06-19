d25e8c02a4dc91ec976bbb1b31c5f2e0604dd875
Sameer <sameer.writes.70@gmail.com>
2022-06-19 12:53:53 +0530

    practice done

---
+++ 18_jun/style.js
@@ -0,0 +1,13 @@
+// ----- Loop Practice -0---
+
+// let i = 2;
+// while (i < 20) {
+//     i++
+//     console.log(i);
+// }
+
+var i = 5;
+do {
+    console.log(i);
+    i++
+} while (i < 30);
---
+++ 19_jun/index.js
@@ -0,0 +1,13 @@
+// for (let i = 0; i <= 21; i++) {
+//     if (i % 2 == 0) {
+
+//         console.log(i);
+//     }
+// }
+// let i = 101;
+// do {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 20);
