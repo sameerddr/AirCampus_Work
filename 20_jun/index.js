55b81a469f91e8d96829f769aa19db49eba6ad45
Sameer <sameer.writes.70@gmail.com>
2022-06-20 17:46:52 +0530

    new practice done

---
+++ 20_jun/index.js
@@ -0,0 +1,32 @@
+// Q1 = Find the greatest of them 
+
+
+//  var a = 2;
+//  var b = 3;
+//  var c = 4;
+//  var d = 5;
+//  var e = 6;
+
+//  if (a > b && a > c && a > d && a > e) {
+//      console.log(a);
+//  } else if (b > a && b > c && b > d && b > e) {
+//      console.log(b);
+//  } else if (c > a && c > b && c > d && c > e) {
+//      console.log(c);
+//  } else if (d > a && d > b && d > c && d > e) {
+//      console.log(d);
+//  } else if (e > a && e > b && e > c && e > d) {
+//      console.log(e);
+//  }
+
+// Q2 =  Sum of the Multiples of the  3 and 5 Under 1000
+
+// var sum = 0;
+// for (let i = 0; i < 1000; i++) {
+//     if (i % 3 == 0 || i % 5 == 0) {
+//         sum = sum + i;
+//     }
+// }
+// console.log(sum);
+
+// Q3 = WAP from loop that will iterate from 0 to 15
