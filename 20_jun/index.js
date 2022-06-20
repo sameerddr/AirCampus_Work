d08d07d87e79743814a06565fca41cc515da996f
Sameer <sameer.writes.70@gmail.com>
2022-06-20 17:53:04 +0530

    practice done

--- 20_jun/index.js
+++ 20_jun/index.js
@@ -29,4 +29,14 @@
 // }
 // console.log(sum);
 
-// Q3 = WAP from loop that will iterate from 0 to 15
+// Q3 = WAP from loop that will iterate from 0 to 15. For each loop that will iterate from 0 to 15 . For each iteration ,  it will check that the current number is even or not and display a message 
+
+for (let i = 0; i < 15; i++) {
+    if (i == 0) {
+        console.log(i + " is even");
+    } else if (i % 2 == 0) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is odd");
+    }
+}
