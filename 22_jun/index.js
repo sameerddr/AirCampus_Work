a401a072fcac179e804eb03907e72b6b8dc2838d
Sameer <sameer.writes.70@gmail.com>
2022-06-22 16:30:31 +0530

    types of function

--- 22_jun/index.js
+++ 22_jun/index.js
@@ -15,9 +15,9 @@
     let mult = a => console.log(a * a);
     mult(4);
 
-    // First Class Function
-    // 1- We can assign The function in the variable and can call it 
-    // 2 - Higher Order Function - In which Fuction Is Passed As a Parameter
+    // Types Of Functions
+    // 1- First Class Function - We can assign The function in the variable and can call it 
+    // 2 - Higher Order Function - In which Function Is Passed As a Parameter or   a function can return a function
     // 3 - First Order Function - In which no argument passed &dont have a return type
 
 
@@ -27,10 +27,38 @@
     }
     sameer();
 
+    // 2 Point Explained
+    function x(para) {
+        para();
+    }
+
+    function y() {
+        console.log("Hello Bhai, Padhai Par Dhyan Laga");
+    }
+
+    x(y);
+
+    //  Here x is a higher Order function & y is a Call Back Function 
+
     // 3 Point Explained
     function first_order(x) {
         console.log("it is first order function");
     }
     first_order();
 
-    // 2 Point Explained
+    // IIFE (Imeediately Invoked Function Expression) - It automatically called the function & if you want to make your variable private You can use it !
+    // Syntax -
+    //     (function() {
+    //         // Code Which You Want To Right
+    //     })();
+
+    (function varshney() {
+        console.log("Kl Exam hai Php Ka");
+    })();
+
+    (function ddr() {
+        var a = 11;
+        console.log(a);
+    })();
+
+    // console.log(a);  here we cant acess the value of the "a"
