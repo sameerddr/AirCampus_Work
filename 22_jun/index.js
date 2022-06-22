556f06b05e653e3e1876c4c57d093315e32e26c2
Sameer <sameer.writes.70@gmail.com>
2022-06-22 15:58:36 +0530

    new files

--- 22_jun/index.js
+++ 22_jun/index.js
@@ -14,3 +14,23 @@
     // 3 Point Explained
     let mult = a => console.log(a * a);
     mult(4);
+
+    // First Class Function
+    // 1- We can assign The function in the variable and can call it 
+    // 2 - Higher Order Function - In which Fuction Is Passed As a Parameter
+    // 3 - First Order Function - In which no argument passed &dont have a return type
+
+
+    // 1 Point Explained 
+    let sameer = function() {
+        console.log("Sameer Is here");
+    }
+    sameer();
+
+    // 3 Point Explained
+    function first_order(x) {
+        console.log("it is first order function");
+    }
+    first_order();
+
+    // 2 Point Explained
