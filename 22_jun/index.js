9f30bc4fe26337c92af3a4e0d9c6694eb1bc91db
Sameer <sameer.writes.70@gmail.com>
2022-06-22 15:27:08 +0530

    new files added

---
+++ 22_jun/index.js
@@ -0,0 +1,16 @@
+    // Arrow Function 
+    // 1- Not Compulsory To Use The Return 
+    // 2- No Need of function Keyword
+    // 3- If there are Only One Variable no need to use "{}" brackets
+
+
+
+    // 1 &n 2 Point Expalained
+    let mul = (x, y) => {
+        console.log(x * y);
+    };
+    mul(2, 3);
+
+    // 3 Point Explained
+    let mult = a => console.log(a * a);
+    mult(4);
