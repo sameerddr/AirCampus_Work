9c6af596d3c019deb2c211d8c13091090ae3ead6
Sameer <sameer.writes.70@gmail.com>
2022-06-27 20:00:13 +0530

    new practice

--- 27_jun/index.js
+++ 27_jun/index.js
@@ -11,3 +11,63 @@ for (var i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+// program to find the factorial of a number
+function factorial(x) {
+
+    // if number is 0
+    if (x === 0) {
+        return 1;
+    }
+
+    // if number is positive
+    else {
+        return x * factorial(x - 1);
+    }
+}
+
+const num = 3;
+
+// calling factorial() if num is non-negative
+if (num > 0) {
+    let result = factorial(num);
+    console.log(`The factorial of ${num} is ${result}`);
+}
+
+// Print Numbers From 1 to 5 Using Recursions
+
+function numbers(b) {
+    if (b == 6) {
+        return;
+    }
+    console.log(b);
+    numbers(b + 1);
+
+}
+numbers(1);
+
+//  code to print a pattern using for loop
+
+function printPattern(range) {
+    for (var i = 1; i <= range; i++) {
+        var str = "";
+        for (var j = 1; j <= i; j++) {
+            str += j + " ";
+        }
+        console.log(str);
+    }
+}
+printPatter(8);
+
+// code to calculate the sum of digits in a number
+
+function digitSum(num) {
+    var sum = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num = Math.floor(num / 10);
+    }
+    return sum;
+}
+console.log(digitSum(4367)); //20
+console.log(digitSum(56349)); //27
