e181c6292328f269efbf6763af2ea206f7f9b877
Sameer <sameer.writes.70@gmail.com>
2022-06-28 19:55:46 +0530

    new question

---
+++ 28jun/index.js
@@ -0,0 +1,19 @@
+getName()
+console.log(fname);
+console.log(lname);
+
+let lname = "vasanth";
+var fname = "surya";
+
+function getName() {
+    printName()
+    console.log("callling");
+
+    function printName() {
+        console.log(place);
+        let age = 10;
+        console.log(age);
+        var place = "chennai";
+
+    }
+}
