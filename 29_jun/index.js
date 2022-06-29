1f4129d7c15bb0779d1df8e916ab7f7d7a43c0c4
Sameer <sameer.writes.70@gmail.com>
2022-06-29 17:46:28 +0530

    new assignment

--- 29_jun/index.js
+++ 29_jun/index.js
@@ -19,7 +19,7 @@
 //     }
 //     console.log(stars);
 // }
-
+// Q4--------------------------------------------------
 let num = 5;
 for (let i = 1; i <= num; i++) {
     let stars = '';
@@ -28,5 +28,20 @@ for (let i = 1; i <= num; i++) {
         stars = stars + i
             // console.log("*");
     }
+    console.log(stars);
+}
+
+// Q5----------------------------------------
+let n = 5;
+let count = 1;
+for (let i = 1; i <= num; i++) {
+    let stars = '';
+    for (let j = 1; j <= i; j++) {
+        stars = stars + count;
+        // stars = " "
+        stars += " "
+        count++;
+    }
+    // stars += "\n"
     console.log(stars);
 }
