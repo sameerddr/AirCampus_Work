7a8edceabd301075803a70c60b0249f80d035270
Sameer <sameer.writes.70@gmail.com>
2022-06-30 22:27:55 +0530

    new file added

--- 29_jun/index.js
+++ 29_jun/index.js
@@ -1,13 +1,13 @@
-// let num = 5;
-// for (let i = 1; i <= num; i++) {
-//     let stars = '';
-//     for (let j = 1; j <= i; j++) {
-//         // stars += '*';
-//         stars = stars + "*"
-//             // console.log("*");
-//     }
-//     console.log(stars);
-// }
+let num = 5;
+for (let i = 1; i <= num; i++) {
+    let stars = '';
+    for (let j = 1; j <= i; j++) {
+        // stars += '*';
+        stars = stars + "*"
+            // console.log("*");
+    }
+    console.log(stars);
+}
 // Q3
 // let num = 5;
 // for (let i = 1; i <= num; i++) {
@@ -20,41 +20,41 @@
 //     console.log(stars);
 // }
 // Q4--------------------------------------------------
-let num = 5;
-for (let i = 1; i <= num; i++) {
-    let stars = '';
-    for (let j = 1; j <= i; j++) {
-        // stars +='*';
-        stars = stars + i
-            // console.log("*");
-    }
-    console.log(stars);
-}
+// let num = 5;
+// for (let i = 1; i <= num; i++) {
+//     let stars = '';
+//     for (let j = 1; j <= i; j++) {
+//         // stars +='*';
+//         stars = stars + i
+//             // console.log("*");
+//     }
+//     console.log(stars);
+// }
 
 // Q5----------------------------------------
-let n = 5;
-let count = 1;
-for (let i = 1; i <= num; i++) {
-    let stars = '';
-    for (let j = 1; j <= i; j++) {
-        stars = stars + count;
-        stars += " "
-        count++;
-    }
-    console.log(stars);
-}
+// let n = 5;
+// let count = 1;
+// for (let i = 1; i <= num; i++) {
+//     let stars = '';
+//     for (let j = 1; j <= i; j++) {
+//         stars = stars + count;
+//         stars += " "
+//         count++;
+//     }
+//     console.log(stars);
+// }
 // Q6-----------------------
-let numm = 5;
-for (let i = 1; i <= numm; i++) {
-    let stars = '';
-    for (let j = 1; j <= i; j++) {
-        if ((i + j) % 2 == 0) {
-            stars = 0
-        } else {
-            stars = 1
-        }
-    }
-    console.log(stars);
-}
+// let numm = 5;
+// for (let i = 1; i <= numm; i++) {
+//     let stars = '';
+//     for (let j = 1; j <= i; j++) {
+//         if ((i + j) % 2 == 0) {
+//             stars = 0
+//         } else {
+//             stars = 1
+//         }
+//     }
+//     console.log(stars);
+// }
 
 // Q7
