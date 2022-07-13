fec79e9e46f88e562d8f6620d58639ccc11d15dc
Sameer <sameer.writes.70@gmail.com>
2022-07-13 16:32:51 +0530

    array matrix

--- 13_jul/2darray.js
+++ 13_jul/2darray.js
@@ -3,17 +3,17 @@
 
 // program to create a two dimensional array all elements Sum 
 
-let matrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
-]
+// let matrix = [
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10, 11, 12],
+//     [13, 14, 15, 16]
+// ]
 
-let rows = matrix.length;
-let columns = matrix[0].length;
+// let rows = matrix.length;
+// let columns = matrix[0].length;
 
-let sum = 0;
+// let sum = 0;
 // for (let i = 0; i < rows; i++) {
 //     for (let j = 0; j < columns; j++) {
 //         sum += matrix[i][j];
@@ -52,11 +52,136 @@ let sum = 0;
 
 // program to find the program to sum the single columns
 
-let start = 1;
+// let start = 0; // it will define the no of coloumn from starting
 
-for (let i = 0; i < rows; i++) {
+// for (let i = 0; i < rows; i++) {
+//     sum += matrix[i][start];
+// }
+// console.log(sum);
 
-    sum += matrix[i][start];
+// program to find the program to sum the single row
 
-}
-console.log(sum);
+
+// let start = 0; // it will define the no of rows from starting
+
+// for (let i = 0; i < rows; i++) {
+//     sum += matrix[start][i];
+// }
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// program to create a two dimensional array
+
+// function twoDimensionArray(a, b) {
+//     let arr = [];
+
+// creating two dimensional array
+//     for (let i = 0; i< a; i++) {
+//         for(let j = 0; j< b; j++) {
+//             arr[i] = [];
+//         }
+//     }
+
+//     // inserting elements to array
+//     for (let i = 0; i< a; i++) {
+//         for(let j = 0; j< b; j++) {
+//             arr[i][j] = j;
+//         }
+//     }
+//     return arr;
+// }
+
+// const x = 2;
+// const y = 3;
+
+// const result = twoDimensionArray(x, y);
+// console.log(result);
+// }
