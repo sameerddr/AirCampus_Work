6db015e6f1f30920a07856d098f640fa2f76cd3c
Sameer <sameer.writes.70@gmail.com>
2022-07-13 15:44:44 +0530

    array matrix

--- 13_jul/2darray.js
+++ 13_jul/2darray.js
@@ -1,7 +1,7 @@
 // The two-dimensional array is a collection of items which share a common name and they are organized as a matrix in the form of rows and columns.
 
 
-// program to create a two dimensional array 
+// program to create a two dimensional array all elements Sum 
 
 let matrix = [
     [1, 2, 3, 4],
@@ -11,12 +11,41 @@ let matrix = [
 ]
 
 let rows = matrix.length;
-let columns = matrix.length;
+let columns = matrix[0].length;
 
 let sum = 0;
-for (let i = 0; i < rows.length; i++) {
-    for (let j = 0; j < columns.length; j++) {
-        sum += matrix[i][j];
-    }
+// for (let i = 0; i < rows; i++) {
+//     for (let j = 0; j < columns; j++) {
+//         sum += matrix[i][j];
+//     }
+// }
+
+
+// IS It Write?
+// for (let i = 0; i < matrix.length; i++) {
+//     for (let j = 0; j < matrix.length; j++) {
+//         sum += matrix[i][j];
+//     }
+// }
+
+
+// console.log(sum);
+
+
+
+// program to calculate the sum of diagonal of above matrix
+
+// 1 method - using two loops
+// for (let i = 0; i < rows; i++) {
+//     for (let j = 0; j < columns; j++) {
+//         if (i === j) // it work for [0][0]  ,[1][1] , [2][2] and so on
+//             sum += matrix[i][j];
+//     }
+// }
+// console.log(sum);
+
+// 2 method - using single loops
+for (let i = 0; i < rows; i++) {
+    sum += matrix[i][i];
 }
 console.log(sum);
