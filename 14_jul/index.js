d82a26c342ece9e3f5c9fab848ba3ca8db837e2e
Sameer <sameer.writes.70@gmail.com>
2022-07-14 12:59:48 +0530

    string methods

---
+++ 14_jul/index.js
@@ -0,0 +1,8 @@
+//    Javascript strings
+
+// In JavaScript, strings are created by surrounding them with quotes. There are three ways you can use quotes.
+
+//strings example
+const name = 'Peter';
+const name1 = "Jack";
+const result = `The names are ${name} and ${name1}`; //Backticks are generally used when you need to include variables or expressions into a string.
