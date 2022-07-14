68c590831eb98a7632693f131fd5f8f601a71e20
Sameer <sameer.writes.70@gmail.com>
2022-07-14 13:08:12 +0530

    string methods

--- 14_jul/index.js
+++ 14_jul/index.js
@@ -6,3 +6,28 @@
 const name = 'Peter';
 const name1 = "Jack";
 const result = `The names are ${name} and ${name1}`; //Backticks are generally used when you need to include variables or expressions into a string.
+
+// JavaScript Strings are immutable -- That means the characters of a string cannot be changed. For example,
+
+// let a = 'hello';
+// a[0] = 'H';
+// console.log(a);
+
+// JavaScript is Case-Sensitive
+// let a = 'a';
+// let b = 'A'
+// console.log(a === b); // false
+
+
+// JavaScript Multiline Strings -- To use a multiline string, you can either use the + operator. For example,
+
+// using the + operator
+const message1 = 'This is a long message ' +
+    'that spans across multiple lines' +
+    'in the code.'
+console.log(message1);
+
+// JavaScript String Length
+
+let z = 'hello';
+console.log(z.length); // 5
