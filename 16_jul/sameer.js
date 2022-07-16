4850203ccbb8d9a464b647881b88f9fbf1bca3d5
Sameer <sameer.writes.70@gmail.com>
2022-07-16 16:02:01 +0530

    new methods

--- 16_jul/sameer.js
+++ 16_jul/sameer.js
@@ -5,3 +5,14 @@ let newArray = numbers.slice(3, 6);
 console.log(newArray);
 
 // Output: [ 7, 11, 13 ]
+
+// -----
+let prime_numbers = [2, 3, 5, 7, 9, 11];
+
+// replace 1 element from index 4 by 13
+let removedElement = prime_numbers.splice(4, 1, 13);
+console.log(removedElement);
+console.log(prime_numbers);
+
+// Output: [ 9 ]
+//         [ 2, 3, 5, 7, 13, 11 ]
