0d6870fe6e60d73420c1ea07d590eda124799b74
Sameer <sameer.writes.70@gmail.com>
2022-07-16 15:47:04 +0530

    new methods

---
+++ 16_jul/sameer.js
@@ -0,0 +1,7 @@
+let numbers = [2, 3, 5, 7, 11, 13, 17];
+
+// create another array by slicing numbers from index 3 to 5
+let newArray = numbers.slice(3, 6);
+console.log(newArray);
+
+// Output: [ 7, 11, 13 ]
