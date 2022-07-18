b4c4bb34003abb977d223a74da2df2d4fc8264c2
Sameer <sameer.writes.70@gmail.com>
2022-07-18 14:57:46 +0530

    test- practice

--- 18_jul/sameer.js
+++ 18_jul/sameer.js
@@ -15,17 +15,22 @@ console.log(linear_search([1, 2, 3, 4, 5, 6], 4));
 
 // Binary Search
 
+
+[1, 2, 3, 4, 5]
+
 function binary_search(arr, key) {
 
     let s = 0; //start
-    let e = arr.length;
+    let e = arr.length; //end 
     while (s <= e) {
         let mid = (s + e) / 2;
     }
     if (arr[mid] == key) {
         return mid;
-    } else if (arr[mid] > key) {
+    } else if (arr[mid] > key) { // for first half  
         e = mid - 1;
+    } else {
+        s = mid + 1; // if key is greater than the mid value we will increase the s value for making new start point
     }
     return -1;
 }
