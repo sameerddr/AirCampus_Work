2c28242af6cc7ae076f83ffce4ad3c8ef74b66ff
Sameer <sameer.writes.70@gmail.com>
2022-07-18 15:04:16 +0530

    test- practice

--- 18_jul/sameer.js
+++ 18_jul/sameer.js
@@ -15,22 +15,19 @@ console.log(linear_search([1, 2, 3, 4, 5, 6], 4));
 
 // Binary Search
 
-
-[1, 2, 3, 4, 5]
-
 function binary_search(arr, key) {
 
     let s = 0; //start
     let e = arr.length; //end 
     while (s <= e) {
         let mid = (s + e) / 2;
-    }
-    if (arr[mid] == key) {
-        return mid;
-    } else if (arr[mid] > key) { // for first half  
-        e = mid - 1;
-    } else {
-        s = mid + 1; // if key is greater than the mid value we will increase the s value for making new start point
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] > key) { // if our key value is  behind the middle element of array 
+            e = mid - 1;
+        } else {
+            s = mid + 1; // if key is after the mid value we will increase the s value for making new start point
+        }
     }
     return -1;
 }
