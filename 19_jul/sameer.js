957a2bd5c1fbdac444be433052dea5d266ec170d
Sameer <sameer.writes.70@gmail.com>
2022-07-19 12:29:18 +0530

    objects

--- 19_jul/sameer.js
+++ 19_jul/sameer.js
@@ -122,3 +122,22 @@ console.log(c.runs());
 
 delete c.position;
 console.log(c);
+
+// -------
+// constructor function
+function Person() {
+    this.name = 'John',
+        this.age = 23,
+
+        this.greet = function() {
+            console.log('hello');
+        }
+}
+
+// create objects
+const person1 = new Person();
+const person2 = new Person();
+
+// access properties
+console.log(person1.name); // John
+console.log(person2.name); // John
