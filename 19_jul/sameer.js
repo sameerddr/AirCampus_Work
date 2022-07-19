cc9206fadb4f17dd1ee219202d0cea2841f0720f
Sameer <sameer.writes.70@gmail.com>
2022-07-19 11:13:36 +0530

    objects

--- 19_jul/sameer.js
+++ 19_jul/sameer.js
@@ -1,4 +1,10 @@
 // objects
+// 1--variables are called property inside the objects
+// 2--functions are called methods inside the objects
+// 3--you can dynamically stores property outside the objects
+// 4--this point the
+// function 's property   // used in constructor function
+
 
 // simple objects 
 
@@ -11,6 +17,8 @@ const course = {
 console.log(course);
 console.log(course.lecture);
 
+
+// ---- Example -2
 const courses = {
     lecture: 100,
     section: 200,
@@ -23,4 +31,6 @@ const courses = {
     }
 }
 
-console.log(courses);
+console.log(courses); // In this the function defination is not working
+console.log(courses.notes);
+courses.enroll();
