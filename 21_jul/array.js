6374a3977867bc8ee83536ae0f2a1a1b4f589c91
Sameer <sameer.writes.70@gmail.com>
2022-07-21 14:04:17 +0530

    revision

--- 21_jul/array.js
+++ 21_jul/array.js
@@ -56,3 +56,20 @@ var obj = {
 }
 
 obj.getName();
+
+var y = 24;
+
+function favFunction() {
+    var x = 667;
+    var anotherFavFunction = function() {
+        console.log(x); // Does not find x inside anotherFavFunction, so looks for variable inside favFunction, outputs 667
+    }
+
+    var yetAnotherFavFunction = function() {
+        console.log(y); // Does not find y inside yetAnotherFavFunction, so looks for variable inside favFunction and does not find it, so looks for variable in global scope, finds it and outputs 24
+    }
+
+    anotherFavFunction();
+    yetAnotherFavFunction();
+}
+favFunction();
