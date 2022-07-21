157ee5471eecc3640cf055d66582fa656d0d3a07
Sameer <sameer.writes.70@gmail.com>
2022-07-21 13:11:28 +0530

    revision

--- 21_jul/array.js
+++ 21_jul/array.js
@@ -31,3 +31,11 @@ function add() {
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 let yes = words.filter(wo => wo.length > 6);
 console.log(yes);
+const message = "JavaScript is a fun programming language.";
+
+// regular expression that checks if message contains 'programming'
+const exp = /programming/;
+
+// check if exp is present in message
+let result = message.match(exp);
+console.log(result);
