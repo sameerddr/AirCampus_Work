8bc1e3807e0a5bbe37a5a95c0774e52b66f98039
Sameer <sameer.writes.70@gmail.com>
2022-07-23 16:17:19 +0530

    interview revsion

--- 23_jul/test.js
+++ 23_jul/test.js
@@ -19,3 +19,21 @@ function display(fn) {
     console.log("saquare is " + fn(5));
 }
 display(square);
+
+// first class function
+
+function greet(name, callback) {
+    console.log('Hi' + ' ' + name);
+    // callback();
+}
+
+// callback function
+function callMe() {
+    console.log('I am callback function');
+}
+
+// passing function as an argument
+greet('Peter', callMe());
+
+
+// callback Function
