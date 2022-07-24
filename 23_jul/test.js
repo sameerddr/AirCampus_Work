c2985a311a996773ce05994a6a13b2b28f37f429
Sameer <sameer.writes.70@gmail.com>
2022-07-24 14:04:33 +0530

    test

--- 23_jul/test.js
+++ 23_jul/test.js
@@ -145,4 +145,24 @@ for (let i = 0; i < arr1.length; i++) {
         delete arr1[i];
     }
 }
+
 console.log(arr1);
+
+
+function binary_search(arr, key) {
+
+    let s = 0; //start
+    let e = arr.length; //end 
+    while (s <= e) {
+        let mid = (s + e) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] > key) { // if our key value is  behind the middle element of array 
+            e = mid - 1;
+        } else {
+            s = mid + 1; // if key is after the mid value we will increase the s value for making new start point
+        }
+    }
+    return -1;
+}
+console.log(binary_search([11, 22, 33, 44, 55, 66], 66));
