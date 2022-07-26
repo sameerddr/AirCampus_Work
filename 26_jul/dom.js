997d172af36dd651847ddbba4ae96aaa2e958e2f
Sameer <sameer.writes.70@gmail.com>
2022-07-26 11:20:19 +0530

    dom

--- 26_jul/dom.js
+++ 26_jul/dom.js
@@ -1,1 +1,17 @@
 console.log("welecome ");
+
+/* 
+element slectors 
+1 . Single element Selector
+2 . Multi element Selector
+
+*/
+// 1 . Single element Selector
+
+let element = document.getElementById("myfirst")
+console.log(element);
+element.style.color = "red";
+// few things which i can get of my element 
+
+// element = element.className;
+// console.log(element);
