9fb49ccb5003bb5ad9be070892246ea95d6af1dd
Sameer <sameer.writes.70@gmail.com>
2022-07-27 13:21:15 +0530

    dom

--- 26_jul/dom2.js
+++ 26_jul/dom2.js
@@ -1,1 +1,10 @@
 console.log("welcome");
+
+let elements = document.createElement("li");
+// console.log(elements);
+
+// add a class name and a id to the created element 
+elements.className = "chid";
+elements, id = "createdli";
+elements.setAttribute("title", "mytitle");
+// console.log(elements);
