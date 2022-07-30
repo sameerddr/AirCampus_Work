1ff3a4e5a9f9023129c4213b46cc240997bdc5c1
Sameer <sameer.writes.70@gmail.com>
2022-07-30 22:21:14 +0530

    assignment16

--- 30_jul/index.js
+++ 30_jul/index.js
@@ -34,3 +34,24 @@ var arr = [243, 45, 23, 356, 3, 5346, 35, 5];
 
 // calling the bubbleSort Function
 bubbleSort(arr)
+
+function selectionSort(inputArr) {
+    let n = inputArr.length;
+
+    for (let i = 0; i < n; i++) {
+        // Finding the smallest number in the subarray
+        let min = i;
+        for (let j = i + 1; j < n; j++) {
+            if (inputArr[j] < inputArr[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            // Swapping the elements
+            let tmp = inputArr[i];
+            inputArr[i] = inputArr[min];
+            inputArr[min] = tmp;
+        }
+    }
+    return inputArr;
+}
