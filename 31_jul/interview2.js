fa9277f769c9883b77147d3e46ed79a8a56ab61c
Sameer <sameer.writes.70@gmail.com>
2022-07-31 11:17:09 +0530

    revision

--- 31_jul/interview2.js
+++ 31_jul/interview2.js
@@ -17,6 +17,8 @@ console.log(sub(3, 2));
 
 
 // ------------------------
+
+const radius = [1, 2, 3, 4];
 const area = function(radius) {
     return Math.PI * radius * radius
 }
@@ -28,3 +30,14 @@ const cirum = function(radius) {
 const diameter = function(radius) {
     return 2 * radius
 }
+
+const calculate = function(radius, logic) {
+    const output = [];
+    for (let i = 0; i < array.length; i++) {
+        output.push(logic(radius[i]))
+    }
+    return output;
+}
+console.log(calculate(radius, area));
+console.log(calculate(radius, cirum));
+console.log(calculate(radius, diameter));
