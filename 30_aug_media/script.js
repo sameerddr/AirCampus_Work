e560f7cc6c6f2999258291180b7ca9beb3bd7f05
Sameer <sameer.writes.70@gmail.com>
2022-08-30 13:12:47 +0530

    media-queries

--- 30_aug_media/script.js
+++ 30_aug_media/script.js
@@ -1,1 +1,7 @@
 console.log("Working");
+
+// For Mobile devices: 320px-480px
+// For Tablets or iPad: 480px - 768px
+// For Laptop or small-size screen: 768px -1024px
+// For Desktop or large-size screen: 1024px -1200px
+// For Extra-large size device: 1200px and more
