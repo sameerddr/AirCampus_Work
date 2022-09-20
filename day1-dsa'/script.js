d61ac619a784f572ff502db6818e8070bf3a5edd
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-09-20 14:37:55 +0530

    dsa

--- day1-dsa'/script.js
+++ day1-dsa'/script.js
@@ -9,6 +9,8 @@ console.log(searchInsert([1, 3, 5, 6], 5));
 console.log(searchInsert([1, 3, 5, 6], 3));
 console.log(searchInsert([1, 3, 5, 6], 6));
 
+// ---------------------------------
+
 var twoSum = function (nums, target) {
   for (let i = 0; i <= nums.length; i++) {
     for (let j = 0; j <= nums.length; j++) {
