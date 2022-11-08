54f8573a59d9587bb19c48190eeae4b95892de6a
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-11-08 16:59:28 +0530

    array questions

--- day1-dsa'/script.js
+++ day1-dsa'/script.js
@@ -25,45 +25,45 @@
 
 // ---------------------------------------------------------------------------------
 // Triplets
-var countGoodTriplets = function (arr, a, b, c) {
-  let count = 0;
-  let n = arr.length;
 
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      for (let k = j + 1; k < n; k++) {
-        if (
-          Math.abs(arr[i] - arr[j]) <= a &&
-          Math.abs(arr[j] - arr[k]) <= c &&
-          Math.abs(arr[i] - arr[k]) <= c
-        )
-          count += 1;
-      }
-    }
-  }
-  return count;
-};
-console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3));
-console.log(countGoodTriplets([1, 1, 2, 2, 3], 0, 0, 1));
+// var countGoodTriplets = function (arr, a, b, c) {
+//   let count = 0;
+//   let n = arr.length;
+
+//   for (let i = 0; i < n; i++) {
+//     for (let j = i + 1; j < n; j++) {
+//       for (let k = j + 1; k < n; k++) {
+//         if (
+//           Math.abs(arr[i] - arr[j]) <= a &&
+//           Math.abs(arr[j] - arr[k]) <= c &&
+//           Math.abs(arr[i] - arr[k]) <= c
+//         )
+//           count += 1;
+//       }
+//     }
+//   }
+//   return count;
+// };
+// console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3));
+// console.log(countGoodTriplets([1, 1, 2, 2, 3], 0, 0, 1));
 
 // -------------------------------------------------------
 
-var smallerNumbersThanCurrent = function (nums) {
-  answer = [];
-  for (let i = 0; i < nums.length; i++) {
-    let count = 0;
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[i] > nums[j]) {
-        count++;
-      }
-    }
-    answer.push(count);
-  }
-  return answer;
-};
+// var smallerNumbersThanCurrent = function (nums) {
+//   answer = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     let count = 0;
+//     for (let j = 0; j < nums.length; j++) {
+//       if (nums[i] > nums[j]) {
+//         count++;
+//       }
+//     }
+//     answer.push(count);
+//   }
+//   return answer;
+// };
 
-console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
+// console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
 
-//
-const mapper = (arr) => arr.map((i) => arr.filter((n) => n < i).length);
-console.log(mapper([8, 1, 2, 2, 3]));
+// const mapper = (arr) => arr.map((i) => arr.filter((n) => n < i).length);
+// console.log(mapper([8, 1, 2, 2, 3]));
