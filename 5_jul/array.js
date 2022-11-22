6c1890116e71375f43ced812063b7e91d967609e
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-11-22 20:32:39 +0530

    DSA question

--- 5_jul/array.js
+++ 5_jul/array.js
@@ -1,53 +1,44 @@
-// Array -- 
+// function firstCapital(str) {
+//     let result = "";
 
-let vechicle = ["car", "scooter", "bus"]; // You Can Use Single & Double Quotes
-console.log(vechicle.length); // .length is used to get the length of given array
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         result += str[i];
+//     }
+//     return result;
+// }
+// console.log(firstCapital("I Love Code"));
 
-console.log(typeof(vechicle)); // Object ??? 
+// function factorial(n) {
+//     if (n == 0 || n < 1) return 1;
+//     else return n * factorial(n - 1);
+// }
+// console.log(factorial(8));
 
-console.log(vechicle[0]); // Car will print as it stored in 0 Index
-console.log(vechicle[1]); //  scooter will print as it stored in 1 Index
-console.log(vechicle[2]); // Bus will print as it stored in 2 Index
-console.log(vechicle[3]); // Undefined bcz we have not given any value at 3 index
+// function longestWord(sen) {
+//     let len = [];
+//     let words = sen.split(" ");
+//     for (let i = 0; i < words.length; i++) {
+//         len.push(words[i].length);
+//     }
+//     let element = Math.max(...len);
+//     return element;
+// }
+// console.log(longestWord("I Love Dogs"));
 
-for (let i = 0; i < vechicle.length; i++) {
-    console.log(vechicle[i]); // priting the elements using for loop
+// function simpleadding(n) {
+//     if (n == 1 || n < 1) {
+//         return 1;
+//     } else {
+//         return n + simpleadding(n - 1);
+//     }
+// }
+// console.log(simpleadding(140));
+
+function firstCapital(str) {
+    let words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        let o = words[i][0].toUpperCase() + words[i].slice(1);
+        return o;
+    }
 }
-
-
-/*  Dry Run For Above For Loop
-
-     i = 0  which is less than Array length, console.log(0); i.e car will print
-     i = 1  which is less than Array length, console.log(1); i.e scooter will print
-     i = 2  which is less than Array length, console.log(2); i.e bus will print
-     i = 3 condition will false , now the loop finsih
-*/
-
-// Differnece Between Object and Arrya 
-/* 
- 
-console.log(type of[]);    will return object
-console.log(type of{});    will return object
-
-console.log([] instanceof Array ); will return true 
-or isarray function 
-
-basical array is indexed elements
-object is named elements 
-
-
-*/
-
-let num = [10, 20];
-console.log(num[0], num[1]);
-
-let copy = num;
-console.log(num[0], num[1]);
-
-copy[0] = 15;
-
-console.log(num, copy);
-num[1] = 30;
-console.log(num, copy);
-
-// You Can Change The value Of The Array elements after declaration & and also be stored in new variable
+console.log(firstCapital("i ran there"));
